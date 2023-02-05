3939250cde4b935259eefc19e02520d8587583f3
Minjeong <rexlam@naver.com>
2023-02-05 18:41:10 +0900

    Listener Study

---
+++ src/main/java/me/with/study/data/repository/ShortUrlRepository.java
@@ -0,0 +1,9 @@
+//package me.with.study.data.repository;
+//
+//import me.with.study.data.entity.ShortUrl;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
+//    ShortUrl findByUrl(String url);
+//    ShortUrl findByOrgUrl(String originalUrl);
+//}
